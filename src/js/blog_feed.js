27504df675ec56ffad8a12f2e0e19dfcd1f6282e
Aaron Leon <aaronleon2018@u.northwestern.edu>
2016-09-22 11:50:31 -0500

    styling blog feed. strip markdown and properly scale thumbnails

--- src/js/blog_feed.js
+++ src/js/blog_feed.js
@@ -3,26 +3,31 @@ ghost.init({ clientId: "ghost-frontend", clientSecret: "617d69bbb120" });
 function onSuccess(data) {
   var feed = $('#blog-feed')[0];
 
-  if (data.posts.length > 0) { $.each(data.posts, function (i, post) { var item =
-      feed.appendChild(document.createElement('div')); $(item).addClass('item');
+  if (data.posts.length > 0) {
+      $.each(data.posts, function (i, post) {
+          var item = feed.appendChild(document.createElement('div')); $(item).addClass('item');
 
-      var thumbnail = $(item).append("<img class='ui image thumbnail' src='http://localhost:2368" +
-        post.image + "' />");
+          var thumbnail = $(item).append('<img class="ui image thumbnail" style="background-image: url(http://localhost:2368' + post.image + ')"/>');
 
-      var description = item.appendChild(document.createElement('div'));
-    $(description).addClass('middle aligned content description');
+          var description = item.appendChild(document.createElement('div'));
+          $(description).addClass('middle aligned content description');
 
-      var title = description.appendChild(document.createElement('a'));
-    title.appendChild(document.createTextNode(post.title)); $(title).addClass('header');
+          var title = description.appendChild(document.createElement('a'));
+          title.appendChild(document.createTextNode(post.title)); $(title).addClass('header');
 
-      var time = description.appendChild(document.createElement('div'));
-    time.appendChild(document.createTextNode(moment(post.created_at).format('llll')));
-    $(time).addClass('meta');
+          var time = description.appendChild(document.createElement('div'));
+          time.appendChild(document.createTextNode(moment(post.created_at).format('llll')));
+          $(time).addClass('meta');
 
-      var bodyDiv = description.appendChild(document.createElement('div')); var bodyText =
-      bodyDiv.appendChild(document.createElement('p'));
-    bodyText.appendChild(document.createTextNode(post.markdown.substring(0,100) + '...')); }); }
-   else { feed.appendChild(document.createTextNode('No News')); }
+          var bodyDiv = description.appendChild(document.createElement('div'));
+          var plainText = markdown.toHTML(post.markdown.substring(0, 100) + '...');
+          var bodyText = $(bodyDiv).append(plainText);
+
+      });
+  }
+  else {
+      feed.appendChild(document.createTextNode('No News'));
+  }
 
 }
 
