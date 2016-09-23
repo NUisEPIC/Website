573084a433f1d4bd27e1894870a69325e2376bb2
Aaron Leon <aaronleon2018@u.northwestern.edu>
2016-09-23 00:25:46 -0500

    wip

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
-  $('#subscribe').modal();         // Initialize modal
+    $('#subscribe-modal').modal();         // Initialize modal
 
-  $('#showSubscribe')              // Show modal on click
-    .click(function() {
-      $('#subscribe').modal('show');
+    $('#show-subscribe').click(function() {
+        $('#subscribe-modal').modal('show');
+    });
+
+    $('#subscribe-form').on('submit', function(event) {
+        event.preventDefault();
+        console.log(getFormData($(this)))
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/list/subscribe/news",
+            dataType: 'json',
+            data: getFormData($(this)),
+            success: function() {
+                $('#subscribe-modal').modal('hide')
+            },
+            error: function(req, status, err) {
+                console.log('ss')
+            console.log( 'something went wrong', status, err  );
+            }
+        })
     });
 });
+
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+
+    });
+
+    return indexed_array;
+}
