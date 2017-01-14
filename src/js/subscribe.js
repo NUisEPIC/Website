0b00e44f43fa2177389c45ec46d7a4e6e8589fd9
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-01-14 14:41:54 -0600

    Update subscription api route

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -11,7 +11,7 @@
             event.preventDefault();
             $.ajax({
                 type: "POST",
-                url: "http://localhost:3000/list/subscribe/news",
+                url: "http://localhost:3000/mail/subscribe",
                 dataType: 'json',
                 data: getFormData($(this)),
                 success: function() {
@@ -23,31 +23,6 @@
                 },
                 error: function() {
                     $('#subscribe-modal').modal('hide');
-                    $('#error>.message').removeClass('hidden');
-                    setTimeout(function() {
-                        $('#error>.message').addClass('hidden');
-                    }, 3000);
-                }
-            });
-        });
-
-        $('#general-member-input').on('submit', function(event) {
-            event.preventDefault();
-            $.ajax({
-                type: "POST",
-                url: "http://localhost:3000/list/subscribe/news",
-                dataType: 'json',
-                data: getFormData($(this)),
-                success: function() {
-                    $('#general-member-input').removeClass('error');
-                    $('#success>.message').removeClass('hidden');
-                    $('#general-member-input').form('clear');
-                    setTimeout(function() {
-                        $('#success>.message').addClass('hidden');
-                    }, 3000);
-                },
-                error: function() {
-                    $('#general-member-input').removeClass('success');
                     $('#error>.message').removeClass('hidden');
                     setTimeout(function() {
                         $('#error>.message').addClass('hidden');
@@ -56,15 +31,4 @@
             });
         });
     });
-
-    function getFormData($form){
-        var unindexed_array = $form.serializeArray();
-        var indexed_array = {};
-
-        $.map(unindexed_array, function(n, i){
-            indexed_array[n['name']] = n['value'];
-        });
-
-        return indexed_array;
-    }
 }());
